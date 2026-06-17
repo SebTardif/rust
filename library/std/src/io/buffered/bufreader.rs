7d0e0b0702fc9252320627dc7fdafffd2f88e5be
Sebastien Tardif <sebtardif@ncf.ca>
2026-06-17 14:38:08 -0700

    fix: use unsigned_abs() in BufReader::seek_relative to avoid i64::MIN panic
    The negation (-offset) panics in debug mode when offset == i64::MIN
    because i64::MIN.abs() exceeds i64::MAX. Using unsigned_abs() computes
    the magnitude safely without overflow for all i64 values.
    
    Fixes SebTardif/rust#159
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- library/std/src/io/buffered/bufreader.rs
+++ library/std/src/io/buffered/bufreader.rs
@@ -303,8 +303,9 @@ impl<R: ?Sized + Seek> BufReader<R> {
     pub fn seek_relative(&mut self, offset: i64) -> io::Result<()> {
         let pos = self.buf.pos() as u64;
         if offset < 0 {
-            if let Some(_) = pos.checked_sub((-offset) as u64) {
-                self.buf.unconsume((-offset) as usize);
+            let abs_offset = offset.unsigned_abs();
+            if let Some(_) = pos.checked_sub(abs_offset) {
+                self.buf.unconsume(abs_offset as usize);
                 return Ok(());
             }
         } else if let Some(new_pos) = pos.checked_add(offset as u64) {
